c75f23074e7e7fe14fd18519ebd1ce5d30c7942c
Sávio Mendes <savio_gato1000@hotmail.com>
2025-11-20 14:22:29 -0300

    Adicionando solução para DominatingSet

---
+++ DominatingSet.java
@@ -0,0 +1,54 @@
+import java.util.*;
+
+public class DominatingSet {
+
+    private Map<Integer, List<Integer>> graph;
+
+    public DominatingSet() {
+        this.graph = new HashMap<>();
+    }
+
+    public void addEdge(int origin, int destination) {
+        graph.putIfAbsent(origin, new ArrayList<>());
+        graph.putIfAbsent(destination, new ArrayList<>());
+        graph.get(origin).add(destination);
+        graph.get(destination).add(origin);
+    }
+
+    public Set<Integer> findDominatingSet() {
+        Set<Integer> dominatingSet = new HashSet<>();
+        Set<Integer> dominatedVertices = new HashSet<>();
+        Integer verticesNumber = graph.size();
+
+        while (dominatedVertices.size() < verticesNumber) {
+            Integer bestVertex = -1;
+            Integer bestCoverage = -1;
+
+            for (Integer vertex : graph.keySet()) {
+                Integer currentCoverage = 0;
+
+                if (!dominatedVertices.contains(vertex)) {
+                    currentCoverage++;
+                }
+
+                for (Integer neighbor : graph.get(vertex)) {
+                    if (!dominatedVertices.contains(neighbor)) {
+                        currentCoverage++;
+                    }
+                }
+
+                if (currentCoverage > bestCoverage) {
+                    bestCoverage = currentCoverage;
+                    bestVertex = vertex;
+                }
+            }
+
+            dominatingSet.add(bestVertex);
+
+            dominatedVertices.add(bestVertex);
+            dominatedVertices.addAll(graph.get(bestVertex));
+        }
+
+        return dominatingSet;
+    }
+}
